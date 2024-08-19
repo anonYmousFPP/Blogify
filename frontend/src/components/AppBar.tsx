3947683cda89175666da6ae48f04e80da9ed32e4
anonymousFPP <abhaygaming1412001@gmail.com>
2024-08-20 01:55:12 +0530

    Your commit message

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Avatar } from './BlogsCard';
 import { Link, useNavigate } from 'react-router-dom';
 
--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -81,7 +81,7 @@ interface LabelledInputType{
 function LabelledInput({label, placeholder, onChange, type}: LabelledInputType){
     return <div>
         <label className="block mb-2 text-sm text-black font-semibold pt-4">{label}</label>
-        <input onChange={onChange} type={type || "text"} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 block w-full p-2.5" placeholder={placeholder} required />
+        <input onChange={onChange} type={type || "text"} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 block w-full p-2.5" placeholder={placeholder} required />
     </div>
 }
 
